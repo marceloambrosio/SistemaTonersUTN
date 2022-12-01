5e6c94d068337c9025a667d793aa31167700ceff
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2022-12-01 20:30:55 -0300

    primer commit

--- registro/urls.py
+++ registro/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('area/<int:id>', views.mostrar_area),
+    path('impresora/<int:id>', views.mostrar_area),
+    path('toner/<int:id>', views.mostrar_area),
 ]
--- registro/views.py
+++ registro/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def mostrar_area(request, id):
+    print(id)
+    return HttpResponse("Hola %s" % id)
