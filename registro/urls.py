8ff24f38387a4c43900f779d1514ac23caba4948
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2022-12-06 20:19:42 -0300

    html

--- registro/urls.py
+++ registro/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('areas/', views.mostrar_areas),
-    path('impresoras/', views.mostrar_impresoras),
-    path('toners/', views.mostrar_toners),
+    path('areas/', views.mostrar_areas, name="areas"),
+    path('impresoras/', views.mostrar_impresoras, name="impresoras"),
+    path('toners/', views.mostrar_toners, name="toners"),
     path('new_area/', views.new_area, name="new_area"),
     path('new_impresora/', views.new_impresora, name="new_impresora"),
     path('new_toner/', views.new_toner, name="new_toner"),
