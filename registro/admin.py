ee684699c035a731ec98193cad3c37d6c6bd77b0
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2022-12-19 18:33:00 -0300

    select admin

--- registro/admin.py
+++ registro/admin.py
@@ -2,6 +2,20 @@ from django.contrib import admin
 from .models import Toner, Area, Impresora
 # Register your models here.
 
-admin.site.register(Toner)
-admin.site.register(Area)
-admin.site.register(Impresora)
+class AreaAdmin(admin.ModelAdmin):
+    search_fields = ('nombre'),
+    ordering = ['nombre']
+
+class TonerAdmin(admin.ModelAdmin):
+    search_fields = ('nombre'),
+    ordering = ['nombre']
+
+class ImpresoraAdmin(admin.ModelAdmin):
+    ordering = ['marca']
+    autocomplete_fields = ['area','toner']
+
+
+
+admin.site.register(Toner, TonerAdmin)
+admin.site.register(Area, AreaAdmin)
+admin.site.register(Impresora, ImpresoraAdmin)
