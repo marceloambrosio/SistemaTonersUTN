1fc666bd64699c87341eb97e18d2fceb68fc02f0
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-01-15 11:21:23 -0300

    template reportes

--- reportes/urls.py
+++ reportes/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.InicioReportes.as_view(), name="inicio_reportes"),
     path('toner_area/', views.TonerPorArea.as_view(), name="toner_area"),
 ]
--- reportes/views.py
+++ reportes/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
+class InicioReportes(TemplateView):
+    template_name = 'reportes.html'
+
 class TonerPorArea(TemplateView):
-    template_name = 'toner_area.html'
+    template_name = 'reportes/toner_area.html'
