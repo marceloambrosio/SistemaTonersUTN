56e1d6d6b6419b7735fa1bf38336a85fd45f46b8
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-02-10 20:34:11 -0300

    filter toner por area

--- reportes/filter.py
+++ reportes/filter.py
@@ -1,10 +1,26 @@
 import django_filters
 from registro.models import Registro
+from django.forms import ModelForm, TextInput, NumberInput, Select, DateInput
 
 class TonerPorAreaFilter(django_filters.FilterSet):
+    fecha = django_filters.DateFromToRangeFilter()
+
     class Meta:
         model = Registro
         fields = {
             #'fecha', 'impresora', 'impresora__toner', 'impresora__area', 'cantidad'
             'impresora__toner': ['exact']
             }
+        widgets = {
+            'impresora__toner': Select(
+                attrs={
+                    'class': 'form-control'
+                }
+            ),
+            'fecha': DateInput(
+                attrs={
+                    'class': 'form-control',
+                    'type': 'date'
+                }
+            )
+        }
--- reportes/views.py
+++ reportes/views.py
@@ -1,12 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, DetailView, CreateView
-from django_filters.rest_framework import DjangoFilterBackend
+from django.views.generic import TemplateView
 from registro.models import Registro
-from django.core.paginator import Paginator
-from django.http import Http404
-from reportes.models import TonerPorArea
 from reportes.filter import TonerPorAreaFilter
-from reportes.forms import TonerPorAreaForm
 
 class InicioReportes(TemplateView):
     template_name = 'reportes.html'
