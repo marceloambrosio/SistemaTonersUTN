bd197ff1e28bd2757ba7e99ad2c1db115ee10b5b
Marcelo Ambrosio <marceambrosio.ok@gmail.com>
2023-02-13 16:14:17 -0300

    filtros template

--- reportes/views.py
+++ reportes/views.py
@@ -10,7 +10,8 @@ def toner_area(request):
     toner_filter = TonerPorAreaFilter(request.GET, queryset=Registro.objects.all())
     context = {
         'form': toner_filter.form,
-        'toner': toner_filter.qs
+        'toner': toner_filter.qs,
+        'cant' : len(toner_filter.qs)
     }
 
     return render(request, 'reportes/toner_area.html', context)
